987f585d598861fd63fcb6f4cfe27250a2d95bc5
Ivan Zizka <zizkaivan@gmail.com>
2019-02-18 23:28:54 +0100

    nefunkcniStavbaStromu

--- app/Main.hs
+++ app/Main.hs
@@ -3,14 +3,20 @@ module Main where
 import Lib
 
 
-data Crumb a = LeftCrumb a (Tree a) | RightCrumb a (Tree a) deriving (Show)
-type Breadcrumbs a = [Crumb a]
 
 -- struktura stromu --
 data Tree a = Empty
     | Node a (Tree a) (Tree a)
     deriving (Show)
+
+--pomocna struktura pro stavbu stromu
+
+data Builder a = None
+    | Content Int (Tree a)
+    deriving (Show)
+
 --vstupmi data --
+
 prefix = [ '-'
          , '+'
          , '6'
@@ -18,17 +24,53 @@ prefix = [ '-'
          , '3'
          ]
 
+freeTree :: Tree Char
+freeTree =
+    Node '-'
+       (Node '*'
+           (Node '+'
+               (Node '6' Empty Empty)
+               (Node '5' Empty Empty)
+           )
+           (Node '/'
+               (Node '7' Empty Empty)
+               (Node '9' Empty Empty)
+           )
+      )
+      (Node '/'
+          (Node '*'
+              (Node '1' Empty Empty)
+              (Node '2' Empty Empty)
+          )
+          (Node '+'
+              (Node '3' Empty Empty)
+              (Node '4' Empty Empty)
+          )
+      )
+buildLeft :: [Char] -> Builder Char -> Builder Char
+buildLeft prefix (Content a t) =
+    if isTrue (prefix!!a) == True then
+      let index = a + 1
+          leftWithContent = buildLeft((last:prefix)(Content a t))
+          newIndex = returnIndex(leftWithContent)
+          right = buildRight()
+
+--buildRight :: [Char] -> Builder Char -> Builder Char
 
 
-goLeft :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goLeft (Node x l r, bs) = (l, LeftCrumb x r:bs)
+returnTree :: Builder Char -> Tree Char
+returnTree (Content _ t) = t
 
-goRight :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goRight (Node x l r, bs) = (r, RightCrumb x l:bs)
+returnIndex :: Builder Char -> Int
+returnIndex (Content a _) = a
 
-goUp :: (Tree a, Breadcrumbs a) -> (Tree a, Breadcrumbs a)
-goUp (t, LeftCrumb x r:bs) = (Node x t r, bs)
-goUp (t, RightCrumb x l:bs) = (Node x l t, bs)
+buildTree :: [Char] -> Tree Char
+buildTree prefix =
+    let leftWithContent = buildLeft( head:prefix (Content 0 Empty))
+        leftTree = returnTree(leftWithContent)
+        leftEnd = returnIndex(leftWithContent)
+        right = returnTree( buildLeft( prefix (Content leftEnd Empty))
+    in Node (head:prefix) leftTree rightTree
 
 --vraci True kdyz je char znamenko --
 isTrue :: Char -> Bool
@@ -40,13 +82,40 @@ isTrue char
     | otherwise = False
 
 
-treeFromList :: [Crumb Char] -> [Char] -> Tree Char
-treeFromList bs prefix = 
+printRight :: Tree Char -> IO()
+printRight (Node a l r) =
+  if isTrue a == True then do
+            printLeft(l)
+            putChar a
+            printRight(r)
+            putChar(')')
+      else
+        let c = [a]
+            output = c ++ " )"
+        in putStr output
 
+--leftTree :: Tree Char -> Tree Char
+--leftTree (Node _ l _) = l
 
-  --treeFromList (x:xs) = Node (treeFromList (filter (<x) xs)) x (treeFromList (filter (>x) xs))
+printLeft :: Tree Char -> IO ()
+printLeft (Node a l r) =
+  if isTrue a == True then do
+            putChar('(')
+            printLeft(l)
+            putChar a
+            printRight(r)
+      else
+        let c = [a]
+            output = "( " ++ c
+        in putStr output
 
-
+printInfix :: Tree Char -> IO ()
+printInfix (Node a l r) = do
+      printLeft(l)
+      putChar a
+      printRight(r)
 
 main :: IO ()
-main = someFunc
+main = do
+  printInfix(buildTree(prefix))
+  putStr ("\n")
