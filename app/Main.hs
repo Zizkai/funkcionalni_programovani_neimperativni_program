5c9486afdff0d34fcd7319121f4e0cc286ddff8c
Ivan Zizka <zizkaivan@gmail.com>
2019-02-20 19:16:41 +0100

    final_komentar

--- app/Main.hs
+++ app/Main.hs
@@ -10,20 +10,25 @@ data Tree a = Empty
     deriving (Show)
 
 --pomocna struktura pro stavbu stromu
-
 data Builder a = None
     | Content Int (Tree a)
     deriving (Show)
 
 --vstupmi data --
-
 prefix = [ '-'
          , '+'
          , '6'
-         , '2'
+         , '5'
+         , '/'
+         , '6'
+         , '-'
+         , '6'
          , '3'
          ]
+-- ^ Zde je mozno editovat vstup
 
+-- testovaci data na rucne vytvorenem stromu
+{-
 freeTree :: Tree Char
 freeTree =
     Node '-'
@@ -47,32 +52,60 @@ freeTree =
               (Node '4' Empty Empty)
           )
       )
+-}
+
+-- stavi levou cast stromu, argumenty jsou list charu a stavebni jednotku Builder Char. Vystupem je opet builder Char.
 buildLeft :: [Char] -> Builder Char -> Builder Char
 buildLeft prefix (Content a t) =
     if isTrue (prefix!!a) == True then
       let index = a + 1
-          leftWithContent = buildLeft((last:prefix)(Content a t))
-          newIndex = returnIndex(leftWithContent)
-          right = buildRight()
+          leftWithContent = buildLeft prefix (Content index t)
+          leftTree = returnTree leftWithContent
+          newIndex = returnIndex leftWithContent
+          rightWithContent = buildRight prefix (Content newIndex t)
+          rightTree = returnTree rightWithContent
+          newIndexRight = returnIndex rightWithContent
+          tree = Node (prefix!!a) leftTree rightTree
+      in (Content newIndexRight tree)
+      else
+        let emptyTree = Node (prefix!!a) Empty Empty
+        in Content (a+1) emptyTree
 
---buildRight :: [Char] -> Builder Char -> Builder Char
+-- stavi pravou cast stromu, argumenty jsou list charu a stavebni jednotku Builder Char. Vystupem je opet builder Char.
+buildRight :: [Char] -> Builder Char -> Builder Char
+buildRight prefix (Content a t) =
+  if isTrue (prefix!!a) == True then
+    let index = a + 1
+        leftWithContent = buildLeft prefix (Content index t)
+        leftTree = returnTree leftWithContent
+        newIndex = returnIndex leftWithContent
+        rightWithContent = buildRight prefix (Content newIndex t)
+        rightTree = returnTree rightWithContent
+        newIndexRight = returnIndex rightWithContent
+        tree = Node (prefix!!a) leftTree rightTree
+    in (Content newIndexRight tree)
+    else
+      let emptyTree = Node (prefix!!a) Empty Empty
+      in Content (a+1) emptyTree
 
-
+-- extrahuje z Builderu jednu polozku a tim je strom
 returnTree :: Builder Char -> Tree Char
 returnTree (Content _ t) = t
 
+-- extrahuje z Builderu jednu polozku a tim je cislo
 returnIndex :: Builder Char -> Int
 returnIndex (Content a _) = a
 
+-- dostava argument typu [Char] (String) nejdrive inicializuje stavbu leveho stromu a pote praveho vraci celkovy strom typu Tree Char
 buildTree :: [Char] -> Tree Char
 buildTree prefix =
-    let leftWithContent = buildLeft( head:prefix (Content 0 Empty))
-        leftTree = returnTree(leftWithContent)
-        leftEnd = returnIndex(leftWithContent)
-        right = returnTree( buildLeft( prefix (Content leftEnd Empty))
-    in Node (head:prefix) leftTree rightTree
+    let leftWithContent = buildLeft prefix (Content 1 Empty)
+        leftTree = returnTree leftWithContent
+        leftEnd = returnIndex leftWithContent
+        right = returnTree (buildRight prefix (Content leftEnd Empty))
+        in Node (head prefix) leftTree right
 
---vraci True kdyz je char znamenko --
+--vraci True kdyz je char znamenko
 isTrue :: Char -> Bool
 isTrue char
     | char == '-' = True
@@ -81,7 +114,7 @@ isTrue char
     | char == '+' = True
     | otherwise = False
 
-
+-- vypisuje pravy strom a vypisuje spravne zavorky. Agrumentem je neco datoveho typu Tree -> vystupem je zapis do konzole
 printRight :: Tree Char -> IO()
 printRight (Node a l r) =
   if isTrue a == True then do
@@ -94,9 +127,7 @@ printRight (Node a l r) =
             output = c ++ " )"
         in putStr output
 
---leftTree :: Tree Char -> Tree Char
---leftTree (Node _ l _) = l
-
+-- vypisuje levy strom a vypisuje spravne zavorky. Agrumentem je neco datoveho typu Tree -> vystupem je zapis do konzole
 printLeft :: Tree Char -> IO ()
 printLeft (Node a l r) =
   if isTrue a == True then do
@@ -109,13 +140,16 @@ printLeft (Node a l r) =
             output = "( " ++ c
         in putStr output
 
+
+-- vypisuje infixovou podobu vyrazu agrumentem je datovy typ Tree vystupem je vypis do konzole
 printInfix :: Tree Char -> IO ()
 printInfix (Node a l r) = do
       printLeft(l)
       putChar a
       printRight(r)
 
+-- main funkce jejiz vystupem je zapis do konzole, take se zde vola funkce printInfix
 main :: IO ()
 main = do
-  printInfix(buildTree(prefix))
-  putStr ("\n")
+  printInfix(buildTree(prefix)) --vypis prefixu, argumentem je funkce buildTree s argumentem prefix
+  putStr ("\n") -- novy radek
